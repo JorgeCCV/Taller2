670adfcf1b768c93acf7ba97c8d22b432fd5f9ee
USUARIO14-PC\Usuario01 <chindark711@gmail.com>
2016-12-21 13:21:06 -0500

    Segunda parte de la clase imagen y animacion

---
+++ imagen_animacion.py
@@ -0,0 +1,24 @@
+from tkinter import *
+import time
+tk=Tk()
+
+####Carga una imagen
+canvas=Canvas(tk,width=400,height=400)
+canvas.pack()
+
+my_image=PhotoImage(file="ball.gif")
+canvas.create_image(0,0,anchor=NW,image=my_image)
+tk.mainloop()
+
+###Animacion
+canvas=Canvas(tk,width=400,height=200)
+canvas.pack()
+canvas.create_polygon(10,10,10,60,50,35)
+
+for x in range(0,60):
+    canvas.move(1,5,0)
+    tk.update()
+    time.sleep(0.05)
+
+tk.mainloop()
+
